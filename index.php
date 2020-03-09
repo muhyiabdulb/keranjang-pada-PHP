e983085583479479e03f971a5171c64419ecb809
muhyiabdulb <59000529+muhyiabdulb@users.noreply.github.com>
2020-03-09 11:09:55 +0700

    keranjan belanja pada PHP

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<head>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<?php 
+require 'connect.php';
+$sql = 'SELECT * FROM product';
+$result = mysqli_query($con, $sql);
+ ?>
+<h2> Pilih items: </h2>
+ <table id="t01">
+ <tr>
+ 	<th>Id</th>
+ 	<th>Name</th>
+ 	<th>Price</th>
+ 	<th>Quantity (in stock)</th>
+ 	<th>Buy</th>
+ </tr>
+ 	<?php while($product = mysqli_fetch_object($result)) { ?> 
+	<tr>
+		<td> <?php echo $product->id; ?> </td>
+		<td> <?php echo $product->name; ?> </td>
+		<td> Rp.<?php echo $product->price; ?> </td>
+		<td> <?php echo $product->quantity; ?> </td>
+		<td> <a href="cart.php?id= <?php echo $product->id; ?> &action=add">Beli</a> </td>
+	</tr>
+	<?php } ?>
+ </table>
+</body>
+
+ </html>
